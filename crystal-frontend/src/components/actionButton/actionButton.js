43a167ecfa17dc47031653474e5137525d3e3478
Belal <b_dirani@hotmail.com>
2019-02-21 21:38:49 +1100

    facebook Login button
    Added facebook login with button click and extract user messages (posts feed)

--- crystal-frontend/src/components/actionButton/actionButton.js
+++ crystal-frontend/src/components/actionButton/actionButton.js
@@ -2,29 +2,74 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import store from '../../store/configureStore';
 import './actionButton.css';
-import { Button } from 'react-bootstrap'
+import { Button } from 'react-bootstrap';
+import FacebookLogin from 'react-facebook-login';
 
-class ActionButton extends Component {
+export default class ActionButton extends Component {
+  state = {
+    isLoggedIn: false,
+    userId: ' ',
+    name: '',
+    email: '',
+    feed: ''
+  }
 
-  handleClick() {
-    if (this.props.socialMedia === 'facebook') {
-      console.log('Querying facebook...')
-    } else if (this.props.socialMedia === 'twitter') {
-      console.log('Querying twitter...')
+  componentClicked = () => console.log('clicked');
+
+  responseFacebook = response => {
+    var posts = response.feed.data
+
+    for (var i = 0; i < posts.length; i++) {
+        let feed = response.feed.data[i].message
+        if(typeof feed !== "undefined"){
+          console.log(feed)
+        }
+      }
+    }
+
+  render() {
+    let fbContent;
+
+    if(this.state.isLoggedIn){
+      fbContent = null;
+    } else {
+      fbContent = (
+    <FacebookLogin
+    appId="375886966477417"
+    autoLoad={true}
+    fields="name,email,feed"
+    onClick={this.componentClicked}
+    callback={this.responseFacebook}
+    />
+);
+
+    }
+      return (
+        <div>{fbContent}</div>
+        // <Button onClick={this.handleClick()}/>
+      )
     }
   }
 
-  render() {
-    return (
-      <Button onClick={this.handleClick()}/>
-    )
-  }
-}
+//   handleClick() {
+//     if (this.props.socialMedia === 'facebook') {
+//       console.log('Querying facebook...')
+//     } else if (this.props.socialMedia === 'twitter') {
+//       console.log('Querying twitter...')
+//     }
+//   }
+//
+//   render() {
+//     return (
+//       <Button onClick={this.handleClick()}/>
+//     )
+//   }
+// }
 
-const mapStateToProps = (state) => {
-  return {
-    socialMedia: state.socialMedia
-  }
-}
-
-export default connect(mapStateToProps)(ActionButton)
+// const mapStateToProps = (state) => {
+//   return {
+//     socialMedia: state.socialMedia
+//   }
+// }
+//
+// export default connect(mapStateToProps)(ActionButton)
