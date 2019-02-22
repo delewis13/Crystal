d67e1aec2b0682d3d360c93fbec5bcd3dc228a9d
Belal <b_dirani@hotmail.com>
2019-02-22 13:36:43 +1100

    Flask AI Integration
    Flask server to call Thanhs Predict function

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
 from flask import send_from_directory
+from WordEmb2 import WordEmbedder
+from personality_predicter import predict_personality_from_post as predict
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
@@ -8,11 +10,16 @@ def init():
     return 'Welcome'
 
 
-@app.route('/long_string', methods = ['POST'])
-def predict():
+@app.route('/api/<long_string>', methods = ['POST'])
+def predict(long_string):
+	# ---- Loading the classifier ---
+	# print('[+] Loading model!');
+	# print();
+    classifier = pickle.load(open('xgboost_model.pickle','rb'))
+    prediction = predict(classifier, long_string, wb)
 
 
-    return
+    return prediction
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
