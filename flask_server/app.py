423bf11382f2cd91f49a6a12c102954017cb7afb
Daniel <daniel.elliott.lewis@gmail.com>
2019-03-02 20:46:28 +1100

    Updated requirements

--- flask_server/app.py
+++ flask_server/app.py
@@ -37,6 +37,6 @@ def predict(long_string):
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8000, debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=True)
   #app.run(host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
   #app.run()
